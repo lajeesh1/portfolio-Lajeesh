d3f07283c6734cdb8f088b46b5ba356682fa68bc
Lajeesh <gokulam.9920@gmail.com>
2024-03-01 12:18:39 +0530

    conatact section finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styles from "./App.module.css";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+
 import Experience from "./components/experience/Experience";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
@@ -11,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Experience />
+      <Contact />
     </div>
   );
 }
