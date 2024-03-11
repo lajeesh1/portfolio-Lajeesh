77766fc6324168477d469c9b3f0bff422968e146
Lajeesh <gokulam.9920@gmail.com>
2024-03-11 14:20:15 +0530

    socil media button added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import fb from "../../../assets/socialMedia/fb.png";
+import youtube from "../../../assets/socialMedia/youtube.png";
+import linkedin from "../../../assets/socialMedia/linkedin.png";
+import insta from "../../../assets/socialMedia/insta.png";
 
 function Hero() {
   return (
@@ -11,9 +15,15 @@ function Hero() {
           Currently a Systems Manager at Sree Gokulam Group, I'm working towards
           becoming a proficient MERN stack developer.
         </p>
-        <a className={styles.contactBtn} href="mailto:lajeeshk@hotmail.com">
+        {/* <a className={styles.contactBtn} href="mailto:lajeeshk@hotmail.com">
           Contact Me
-        </a>
+        </a> */}
+        <div className={styles.socialMedia}>
+          <img src={fb} alt="facebook" />
+          <img src={youtube} alt="youtube" />
+          <img src={linkedin} alt="linkdin" />
+          <img src={insta} alt="insta" />
+        </div>
       </div>
       <img
         className={styles.heroImg}
