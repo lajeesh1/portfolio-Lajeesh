7be86952a5d1e2ae24ae73f921e39ed8c1b234f3
Lajeesh <gokulam.9920@gmail.com>
2024-03-11 15:00:09 +0530

    socil media button added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 import fb from "../../../assets/socialMedia/fb.png";
-import youtube from "../../../assets/socialMedia/youtube.png";
+import git from "../../../assets/socialMedia/git.png";
 import linkedin from "../../../assets/socialMedia/linkedin.png";
 import insta from "../../../assets/socialMedia/insta.png";
 
@@ -20,7 +20,7 @@ function Hero() {
         </a> */}
         <div className={styles.socialMedia}>
           <img src={fb} alt="facebook" />
-          <img src={youtube} alt="youtube" />
+          <img src={git} alt="git" />
           <img src={linkedin} alt="linkdin" />
           <img src={insta} alt="insta" />
         </div>
