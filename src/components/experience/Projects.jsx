020fc410d31d37c93f2719c3308fc1e850fc95b6
Lajeesh <gokulam.9920@gmail.com>
2024-10-19 15:26:20 +0530

    project updated-3 191024

--- src/components/experience/Projects.jsx
+++ src/components/experience/Projects.jsx
@@ -1,31 +1,73 @@
+// import React from "react";
+// import skills from "../../Data/skills.json";
+// import history from "../../Data/history.json";
+// import { getImageUrl } from "../../utils";
+// import styles from "./Experience.module.css";
+// function Projects() {
+//   return (
+//     <section className={styles.container} id="experience">
+//       <h3 className={styles.title}>Projects</h3>
+//       <div className={styles.content}>
+//         <ul className={styles.history}>
+//           {history.map((history, id) => (
+//             <li className={styles.historyItem} key={id}>
+              
+//               <img
+//                 src={getImageUrl(history.imageSrc)}
+//                 alt={`${history.app} logo`}
+//               />
+//               <div className={styles.historyItemDetails}>
+//                 <p>{`${history.role}`}</p>
+//                 <h3 className={styles.company}>{` ${history.organisation}`}</h3>
+//                 <p>{`${history.startDate}  ${history.endDate}`}</p>
+//                 <ul>
+//                   {history.experiences.map((experience, id) => (
+//                     <li key={id}>{experience}</li>
+//                   ))}
+//                 </ul>
+//               </div> 
+//               <div className={styles.blur}></div>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Projects;
+
 import React from "react";
 import skills from "../../Data/skills.json";
 import history from "../../Data/history.json";
 import { getImageUrl } from "../../utils";
 import styles from "./Experience.module.css";
+
 function Projects() {
   return (
     <section className={styles.container} id="experience">
       <h3 className={styles.title}>Projects</h3>
       <div className={styles.content}>
         <ul className={styles.history}>
-          {history.map((history, id) => (
+          {history.map((item, id) => (
             <li className={styles.historyItem} key={id}>
-              <img
-                src={getImageUrl(history.imageSrc)}
-                alt={`${history.app} logo`}
-              />
-              <div className={styles.historyItemDetails}>
-                <p>{`${history.role}`}</p>
-                <h3 className={styles.company}>{` ${history.organisation}`}</h3>
-                <p>{`${history.startDate}  ${history.endDate}`}</p>
-                <ul>
-                  {history.experiences.map((experience, id) => (
-                    <li key={id}>{experience}</li>
-                  ))}
-                </ul>
-              </div> 
-              <div className={styles.blur}></div>
+              <a href={`${item.link}`} target="_blank" rel="noopener noreferrer" className={styles.historyLink}>
+                <img
+                  src={getImageUrl(item.imageSrc)}
+                  alt={`${item.app} logo`}
+                />
+                <div className={styles.historyItemDetails}>
+                  <p>{`${item.role}`}</p>
+                  
+                  <p>{`${item.startDate}  ${item.endDate}`}</p>
+                  <ul>
+                    {item.experiences.map((experience, idx) => (
+                      <li key={idx}>{experience}</li>
+                    ))}
+                  </ul>
+                </div>
+                
+              </a>
             </li>
           ))}
         </ul>
@@ -35,3 +77,4 @@ function Projects() {
 }
 
 export default Projects;
+
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,3 +1,44 @@
+// import React from "react";
+// import styles from "./Hero.module.css";
+// import { getImageUrl } from "../../utils";
+// import fb from "../../../assets/socialMedia/fb.png";
+// import git from "../../../assets/socialMedia/git.png";
+// import linkedin from "../../../assets/socialMedia/linkedin.png";
+// import insta from "../../../assets/socialMedia/insta.png";
+
+// function Hero() {
+//   return (
+//     <section className={styles.container}>
+//       <div className={styles.content}>
+//         <h1 className={styles.title}>Hi I'm <br/> Lajeesh</h1>
+//         <p className={styles.description}>
+//           Currently a Systems Manager at Sree Gokulam Group, I'm working towards
+//           becoming a proficient MERN stack developer.
+//         </p>
+//         {/* <a className={styles.contactBtn} href="mailto:lajeeshk@hotmail.com">
+//           Contact Me
+//         </a> */}
+//         <div className={styles.socialMedia}>
+//           <img src={git} alt="git" />
+//           <img src={fb} alt="facebook" />
+
+//           <img src={linkedin} alt="linkdin" />
+//           <img src={insta} alt="insta" />
+//         </div>
+//       </div>
+//       <img
+//         className={styles.heroImg}
+//         src={getImageUrl("hero/heroImage.png")}
+//         alt=""
+//       />
+//       <div className={styles.topBlur}></div>
+//       <div className={styles.bottomBlur}></div>
+//     </section>
+//   );
+// }
+
+// export default Hero;
+
 import React from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
@@ -10,7 +51,7 @@ function Hero() {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi I'm <br/> Lajeesh</h1>
+        <h1 className={styles.title}>Hi I'm <br/><span>Lajeesh</span> </h1>
         <p className={styles.description}>
           Currently a Systems Manager at Sree Gokulam Group, I'm working towards
           becoming a proficient MERN stack developer.
@@ -19,12 +60,14 @@ function Hero() {
           Contact Me
         </a> */}
         <div className={styles.socialMedia}>
-          <img src={git} alt="git" />
+          <a href="https://github.com/lajeesh1?tab=repositories" target="_blank" rel="noopener noreferrer">
+            <img src={git} alt="git" />
+          </a>
           <img src={fb} alt="facebook" />
-
-          <img src={linkedin} alt="linkdin" />
+          <img src={linkedin} alt="linkedin" />
           <img src={insta} alt="insta" />
         </div>
+        <button className="contact-button">Contact Me</button>
       </div>
       <img
         className={styles.heroImg}
